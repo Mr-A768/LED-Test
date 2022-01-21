263765b91fb47b711ca7a33bbf906d51c43b33b9
AndreaDiehl <93095800+AndreaDiehl@users.noreply.github.com>
2022-01-20 19:30:06 -0600

    Corrections

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,11 +1,12 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
+ 
 package frc.robot;
-
-//why aren't you working
-
+ 
+ 
+     
+ 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -13,7 +14,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj.XboxController;
 import com.ctre.phoenix.CANifier;
-
+ 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -22,13 +23,17 @@ import com.ctre.phoenix.CANifier;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-
+ 
   private RobotContainer m_robotContainer;
-
+ 
   public static XboxController controller;
   public static AddressableLED m_led;
   public static AddressableLEDBuffer m_ledBuffer;
-
+ 
+  int x = 29;
+  int x2 = 30;
+  int y = 0;
+ 
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -38,21 +43,23 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-
+ 
     m_led = new AddressableLED(9);
-
+ 
+   
+ 
     // Reuse buffer
     // Default to a length of 60, start empty output
     // Length is expensive to set, so only set it once, then just update data
     m_ledBuffer = new AddressableLEDBuffer(30);
     m_led.setLength(m_ledBuffer.getLength());
-
+ 
     // Set the data
     m_led.setData(m_ledBuffer);
     m_led.start();
-
+ 
   }
-
+ 
   /**
    * This function is called every robot packet, no matter the mode. Use this for items like
    * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
@@ -68,29 +75,29 @@ public class Robot extends TimedRobot {
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
   }
-
+ 
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
   public void disabledInit() {}
-
+ 
   @Override
   public void disabledPeriodic() {}
-
+ 
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
+ 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
     }
   }
-
+ 
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {}
-
+ 
   @Override
   public void teleopInit() {
     // This makes sure that the autonomous stops running when
@@ -100,64 +107,77 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+ 
+   
+ 
   }
-
+ 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-
+ 
     controller = new XboxController(0);
-
-    if (controller.getAButtonPressed()) {
-
-      if (true) {
-
-        for (var a = 0; a < (m_ledBuffer.getLength()) - 20; a++) {
-
-         // Sets the specified LED to the RGB values for red
-         m_ledBuffer.setRGB(a, 255, 0, 0);
-
-        }
-     
-       m_led.setData(m_ledBuffer);
-
-        for (var b = 10; b < m_ledBuffer.getLength() - 10; b++) {
-
-          // Sets the specified LED to the RGB values for red
-         m_ledBuffer.setRGB(b, 0, 255, 0);
-
-        }
-    
-       m_led.setData(m_ledBuffer);
-
-        for (var c = 20; c < m_ledBuffer.getLength(); c++) {
-
-         // Sets the specified LED to the RGB values for red
-         m_ledBuffer.setRGB(c, 0, 0, 255);
-
-       }
-  
-       m_led.setData(m_ledBuffer);
-
+ 
+    for (var bg = 0; bg < (m_ledBuffer.getLength()); bg++) {
+ 
+      // Sets the specified LED to the RGB values for red
+      m_ledBuffer.setRGB(bg, 255, 255, 255);
+ 
+    }
+ 
+    if (controller.getAButton()) {
+ 
+      if (x < 15){
+        x += 15;
       }
-
-      
-		}
-
-	
-
+ 
+      if (x2 < 16){
+        x2 += 15;
+      }
+ 
+      if (y > 15){
+        y -= 15;
+      }
+   
+      for (var a = 0; a < (m_ledBuffer.getLength()) - x; a++) {
+ 
+       // Sets the specified LED to the RGB values for red
+       m_ledBuffer.setRGB(a, 255, 0, 255);
+ 
+     }
+ 
+     for (var b = 0; b < (m_ledBuffer.getLength()) - x2 ; b++) {
+ 
+      // Sets the specified LED to the RGB values for red
+      m_ledBuffer.setRGB(b, 255, 255, 255);
+ 
+    }
+ 
+      m_led.setData(m_ledBuffer);
+ 
+      x --;
+      x2 --;
+      y ++;
+ 
+     
+    }
+ 
+ 
+ 
   }
-
-
-  
-
+ 
+ 
+ 
+ 
   @Override
   public void testInit() {
     // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
   }
-
+ 
   /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
 }
+ 
+
